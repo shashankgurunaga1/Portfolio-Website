74781e63681c49ac2bfdaca95ce282e22e43ce4f
Shashank Gurunaga <98585901+shashankgurunaga1@users.noreply.github.com>
2022-12-01 01:36:31 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = (props) =>{
   return (
     <nav className="navbar py-3 navbar-expand-lg navbar-dark bg-primary ">
   <div className="container-fluid">
-    
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
     
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
