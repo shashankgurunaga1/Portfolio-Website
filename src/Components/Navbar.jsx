3f017a01f5d4993f1c3ef601ec4a72275c0838b6
Shashank Gurunaga <98585901+shashankgurunaga1@users.noreply.github.com>
2022-12-15 20:06:36 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = (props) =>{
   return (
-    <nav className="navbar py-3 navbar-expand-lg navbar-dark bg-primary ">
+    <nav className="navbar py-3 navbar-expand-lg navbar-dark bg-dark ">
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
